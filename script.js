a69dccf445444d514775bcd8092cd227ffcc24bc
AustinSaysHello <austincarvey@vivaldi.net>
2023-09-19 18:29:16 +0000

    added a wrapper for axios to play better with alpine

--- script.js
+++ script.js
@@ -17,6 +17,48 @@ var doGet = function(url) {
     });
 }
 
-var testFunction = function(str) {
-  return str;
+var doGetPromise = function(url){
+  var myResponse = "";
+  return new Promise(function (resolve,reject) {
+    axios.get(url)
+    .then(function(response) {
+      myResponse = response
+    })
+    .catch(function(error) {
+      myResponse = error
+    })
+    if(myResponse != ""){
+      resolve(myResponse);
+    }
+    else{
+      reject("it didn't work");
+    }
+  })
+}
+
+var doGetPromise2 = function (url){
+  return new Promise (function (resolve, reject) {
+    /// code
+    if( url != ""){
+      resolve(url)
+    }
+    else{
+      reject("invalid url")
+    }
+  })
+}
+
+var doGetPromise3 = function (url){
+  return new Promise (function (resolve, reject) {
+    /// code
+    axios.get(url)
+    .then(function(response) {
+      // handle success
+      resolve(response)
+    })
+    .catch(function(error) {
+      // handle error
+      reject(error)
+    })
+  })
 }
