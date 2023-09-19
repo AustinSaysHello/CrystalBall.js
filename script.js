995b06acedd15b3c493fd3a1ef6ed0f09073bdcb
AustinSaysHello <austincarvey@vivaldi.net>
2023-09-19 16:53:37 +0000

    managed to get a get request to work!

---
+++ script.js
@@ -0,0 +1,14 @@
+//console.log("Hello, world!")
+
+axios.get("https://catfact.ninja/fact")
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
