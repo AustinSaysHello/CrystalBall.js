749f11c89585512f97fe46f732f5c3936324d498
AustinSaysHello <austincarvey@vivaldi.net>
2023-09-22 19:30:22 +0000

    added formatting to html, and cleaned up functions in javascript

--- script.js
+++ script.js
@@ -1,55 +1,6 @@
 var testObj = "";
-var doGet = function(url) {
-  axios.get(url)
-    .then(function(response) {
-      // handle success
-      console.log("success");
-      console.log(response);
-      return "success";
-    })
-    .catch(function(error) {
-      // handle error
-      console.log("error");
-      console.log(error);
-      return "error";
-    })
-    .finally(function() {
-      // always executed
-    });
-}
 
 var doGetPromise = function(url) {
-  var myResponse = "";
-  return new Promise(function(resolve, reject) {
-    axios.get(url)
-      .then(function(response) {
-        myResponse = response
-      })
-      .catch(function(error) {
-        myResponse = error
-      })
-    if (myResponse != "") {
-      resolve(myResponse);
-    }
-    else {
-      reject("it didn't work");
-    }
-  })
-}
-
-var doGetPromise2 = function(url) {
-  return new Promise(function(resolve, reject) {
-    /// code
-    if (url != "") {
-      resolve(url)
-    }
-    else {
-      reject("invalid url")
-    }
-  })
-}
-
-var doGetPromise3 = function(url) {
   return new Promise(function(resolve, reject) {
     /// code
     axios.get(url)
