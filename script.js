505f6b65cbae279c5b1aabe1ded0b58559c11381
AustinSaysHello <austincarvey@vivaldi.net>
2023-09-27 20:44:34 +0000

    added logic to try contentful API, hiding API key

--- script.js
+++ script.js
@@ -15,6 +15,27 @@ var doGetPromise = function(url) {
         reject(error)
       })
   })
-}
+};
 
 // have been using this url to test: https://catfact.ninja/fact
+
+// trying the contenful API after this 
+// base url: https://cdn.contentful.com
+// spaceID: hw2cgkmjcfcb
+// environemtID: master
+
+var space = "hw2cgkmjcfcb";
+var environment = "master";
+
+var contentObj = "";
+
+
+var apiUrl = `https://cdn.contentful.com/spaces/${space}/environments/${environment}/entries?access_token=${accessToken}`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(function (response) {
+  contentObj = response;
+  console.log(contentObj.data.items)
+});
+
